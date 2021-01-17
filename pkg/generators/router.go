576d078f584b09fb3b959fa980b847b2536d4380
Nils Måsén <nils@piksel.se>
2021-01-17 02:37:25 +0100

    feat(telegram): add MVP generator using bot API

--- pkg/generators/router.go
+++ pkg/generators/router.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"github.com/containrrr/shoutrrr/pkg/generators/basic"
 	"github.com/containrrr/shoutrrr/pkg/generators/xouath2"
+	"github.com/containrrr/shoutrrr/pkg/services/telegram"
 	t "github.com/containrrr/shoutrrr/pkg/types"
 	"strings"
 )
 
 var generatorMap = map[string]func() t.Generator{
-	"basic": func() t.Generator { return &basic.Generator{} },
-	"oauth2": func() t.Generator { return &xouath2.Generator{} },
+	"basic":    func() t.Generator { return &basic.Generator{} },
+	"oauth2":   func() t.Generator { return &xouath2.Generator{} },
+	"telegram": func() t.Generator { return &telegram.Generator{} },
 }
 
 func NewGenerator(identifier string) (t.Generator, error) {
